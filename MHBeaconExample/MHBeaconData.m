4bcb47ae4bb2f15c615b77c64ffc97342fb401e6
Jaden Geller <jaden.geller@gmail.com>
2014-01-18 02:11:15 -0500

    Implemented dictionary value

--- MHBeaconExample/MHBeaconData.m
+++ MHBeaconExample/MHBeaconData.m
@@ -7,11 +7,42 @@
 //
 
 #import "MHBeaconData.h"
+#import <CoreBluetooth/CoreBluetooth.h>
 
 @implementation MHBeaconData
 
 -(NSDictionary*)dictionaryValue{
+    NSMutableDictionary *dictionary = [NSMutableDictionary dictionary];
     
+    if (self.localNameKey) {
+        [dictionary setObject:self.localNameKey forKey:CBAdvertisementDataLocalNameKey];
+    }
+    if (self.manufacturerDataKey) {
+        [dictionary setObject:self.manufacturerDataKey forKey:CBAdvertisementDataManufacturerDataKey];
+    }
+    if (self.serviceDataKey) {
+        [dictionary setObject:self.manufacturerDataKey forKey:CBAdvertisementDataManufacturerDataKey];
+    }
+    if (self.dataServiceUUIDsKey) {
+        [dictionary setObject:self.manufacturerDataKey forKey:CBAdvertisementDataManufacturerDataKey];
+    }
+    if (self.dataOverflowServiceUUIDsKey) {
+        [dictionary setObject:self.manufacturerDataKey forKey:CBAdvertisementDataManufacturerDataKey];
+    }
+    if (self.dataTxPowerLevelKey) {
+        [dictionary setObject:self.manufacturerDataKey forKey:CBAdvertisementDataManufacturerDataKey];
+    }
+    if (self.dataIsConnectable) {
+        [dictionary setObject:self.manufacturerDataKey forKey:CBAdvertisementDataManufacturerDataKey];
+    }
+    if (self.dataSolicitedServiceUUIDsKey) {
+        [dictionary setObject:self.manufacturerDataKey forKey:CBAdvertisementDataManufacturerDataKey];
+    }
+    if (self.manufacturerDataKey) {
+        [dictionary setObject:self.manufacturerDataKey forKey:CBAdvertisementDataManufacturerDataKey];
+    }
+    
+    return dictionary;
 }
 
 @end
