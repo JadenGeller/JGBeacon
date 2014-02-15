d1b56938679feffe1b2d03819b9922e41a0913f7
Jaden Geller <jaden.geller@gmail.com>
2014-02-15 06:24:24 -0500

    OMG EVERYTHING WORKS FLAWLESSLY
    LIKE EVEN ON OS X I MADE AN OS X APP AND ITS JUST LIKE :O AND IM LIKE
    :D AND YEAH SO THIS IS GREAT SO :))))))))

---
+++ JGBeaconExample_OSX/JGBeaconExample/JGAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  JGAppDelegate.h
+//  JGBeaconExample
+//
+//  Created by Jaden Geller on 2/15/14.
+//  Copyright (c) 2014 Jaden Geller. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface JGAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
